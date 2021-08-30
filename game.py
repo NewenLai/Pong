2d300e3802042e707f0530edda06804770405eb9
DESKTOP-RKQ3NO9\John <blacktaru@gmail.com>
2021-08-30 22:32:13 +0200

    Segunda subida tras vaciones Pong

--- game.py
+++ game.py
@@ -3,14 +3,25 @@ from random import randrange
 
 tamanno = (800, 600)
 
-class Bola():
-
-    def __init__(self, x, y, w, h, color = (255, 255, 255)):
+class Movil():
+    def __init__(self, x, y, h, w, color=(255, 255, 255)):
         self.x = x
         self.y = y
         self.w = w
         self.h = h
         self.color = color
+
+    def actualizar(self):
+        pass
+
+    def Dibuja(self, lienzo):
+        pg.draw.rect(lienzo, self.color, pg.Rect(self.x, self.y, self.w, self.h))
+
+class Bola(Movil):
+
+    def __init__(self, x, y, color = (255, 255, 255)):
+        super().__init__(x,y, 20, 20, color) #misma instruccion que la de raqueta Movil.init
+   
         self.movHor = True
         self.movVert = True
         self.incremento_x = 5
@@ -50,16 +61,29 @@ class Bola():
             print(self.x)
             print(self.incremento_y)
             self.incremento_y *= -1
+
+class Raqueta(Movil):
+    def __init__(self, x, y, color=(255, 255, 255)):
+        Movil.__init__(self, x, y, 80, 10, color)
         
+
+
+
 class Game():
 
     def __init__(self, tamanno):
         self.pantalla = pg.display.set_mode(tamanno)
         self.reloj = pg.time.Clock() #limitar a x fps
-        
-        #self.bola = Bola(tamanno[0]//2-10, tamanno[1]//2-10, 20, 20)
-        #self.bola2 = Bola(tamanno[0]//2-10, tamanno[1]//2-10,15,15, ( 255,0,0))
-        self.bolas = []
+
+        self.todos =[]
+
+        self.player1 = Raqueta(30,(tamanno[1]//2)-40)
+        self.player2 = Raqueta(tamanno[0]-30,(tamanno[1]//2) -40,)
+
+        self.todos.append(self.player1)
+        self.todos.append(self.player2)
+
+        """
         for i in range(10):
             tamanyo = randrange(10, 41)
             bola = Bola(randrange(0, tamanno[0]), randrange(0, tamanno[1]), tamanyo, tamanyo, (randrange(256),randrange(256), randrange(256)))
@@ -68,29 +92,31 @@ class Game():
             bola.movVert = randrange(2) == 1
 
             self.bolas.append(bola)
+        """
 
+        bola = Bola(tamanno[0]//2 - 10, tamanno[1]//2-10, (255, 255, 0))
+        self.todos.append(bola)
 
     def bucle_ppal(self):
         game_over = False
         pg.init()
-        mov = False
+
+       
         while not game_over:
             self.reloj.tick(60) #limita a 60 fps
+
             eventos = pg.event.get()
             for evento in eventos:
                 if evento.type == pg.QUIT:
                     game_over =True
             
-            for i in range(10):
-                self.bolas[i].actualizar()
-            #self.bola.actualizar()            
-            #self.bola2.actualizar()
+            for movil in self.todos:
+                movil.actualizar()
 
             self.pantalla.fill((0, 0, 0))
-            for bola in self.bolas:
-                pg.draw.rect(self.pantalla, bola.color, pg.Rect(bola.x, bola.y, bola.w, bola.h))
-            #pg.draw.rect(self.pantalla, self.bola.color, pg.Rect(self.bola.x, self.bola.y, self.bola.w, self.bola.h))
-            #pg.draw.rect(self.pantalla, self.bola2.color, pg.Rect(self.bola2.x, self.bola2.y, self.bola2.w, self.bola2.h))
+
+            for movil in self.todos:
+                movil.Dibuja(self.pantalla)
 
             pg.display.flip()
             
